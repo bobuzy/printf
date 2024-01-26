71bbf035de0950be0f6e39801a4809a56f4a9e9e
Benedict <bobuzytech@gmail.com>
2024-01-26 10:56:37 -0800

    Create header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int printchar(char c);
+int printstring(char *str);
+
+#endif
