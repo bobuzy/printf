08506b325f0a13b2eb4548b9b74ca0772de22d85
Benedict <bobuzytech@gmail.com>
2024-01-29 14:19:24 -0800

    Handle c,s,d,i and % format specifiers

--- _printf.c
+++ _printf.c
@@ -9,17 +9,13 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0, outputlength = 0;
-
 	va_list args;
+	int i = 0;
+	int output = 0;
 
 	va_start(args, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-	{
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (format == NULL)
 	{
 		return (-1);
 	}
@@ -28,94 +24,21 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			outputlength += printchar(format[i]);
+			output += write(1, &format[i], 1);
 		}
 		else
 		{
-			if (format[i + 1] == '%')
+			get_spec f = {0};
+
+			f.f = check_fmt_spec(format[i + 1]);
+
+			if (f.f != NULL)
 			{
-				outputlength += printchar('%');
-			}
-			else if (format[i + 1] == 'c')
-			{
-				outputlength += printchar(va_arg(args, int));
-			}
-			else if (format[i + 1] == 's')
-			{
-				outputlength += printstr(va_arg(args, char*));
-			}
-			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
-			{
-				outputlength += printdig(va_arg(args, int));
+				output += f.f(args);
 			}
 			i++;
 		}
 		i++;
 	}
-	va_end(args);
-	return (outputlength);
-}
-
-/**
- * printchar - Print a character into standard output
- * @c: The character to be printed
- *
- * Return: 1, if successful
- */
-
-int printchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
- * printstr - Print a string into standard output
- * @str: The string to be printed
- *
- * Return: The length of the string
- */
-
-int printstr(char *str)
-{
-	int outputlength = 0;
-
-	for (; *str != '\0'; str++)
-	{
-		printchar(*str);
-		outputlength += 1;
-	}
-	return (outputlength);
-}
-
-
-/**
- * printdig - Print a digit recursively
- * @num: The number to be printed
- *
- * Return: The length of the digit printed
- */
-
-int printdig(int num)
-{
-	unsigned int i;
-	int outputlength = 0;
-
-	i = num;
-
-	if (num < 0)
-	{
-		printchar('-');
-		i = -num;
-	}
-
-	if (i / 10 != 0)
-	{
-		printdig(i / 10);
-	}
-
-	printchar((i % 10) + '0');
-	outputlength += 1;
-
-	return (outputlength);
+	return (output);
 }
