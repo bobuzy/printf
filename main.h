19963722d7ab2f03a6fdab2b9d980e588be07c45
Benedict <bobuzytech@gmail.com>
2024-01-29 14:14:36 -0800

    Update header file

--- main.h
+++ main.h
@@ -2,11 +2,23 @@
 #define HEADER_FILE
 
 #include <stdarg.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+typedef struct fmt
+{
+	char abc;
+	int (*f)(va_list);
+} get_spec;
 
 int _printf(const char *format, ...);
-int printchar(char c);
-int printstr(char *str);
-int printdig(int num);
+int (*check_fmt_spec(char s))(va_list);
+int printchar(va_list);
+int printstring(va_list);
+int printpercent(va_list args);
+int printdigit(va_list args);
+int printdig(int n);
 
 #endif
