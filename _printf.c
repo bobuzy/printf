4bcadd57a6964921b3475036924cf9dfc1e3f947
Benedict <bobuzytech@gmail.com>
2024-01-30 07:47:52 -0800

    Consider edge cases

--- _printf.c
+++ _printf.c
@@ -19,6 +19,14 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
+	if (!format || (format[0] == '%' && !format[1]))
+	{
+		return (-1);
+	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
 
 	while (format[i])
 	{
